95ab71fcfbdb3178a4dc3247bd56961aab785946
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2025-10-22 18:25:20 -0400

    Categorical Cross Entropy

---
+++ loss/categorial_cross_entropy.py
@@ -0,0 +1,20 @@
+import jax.numpy as jnp
+from jax import Array
+
+from loss import Loss
+
+class CategorialCrossEntropy(Loss):
+    def __init__(self, min_val:float = 1e-8, max_val:float = 1-1e-8):
+        self.min_val = min_val
+        self.max_val = max_val
+    
+    def calculate_loss(self, y_pred: Array, y_true: Array)-> Array:
+        y_pred_clipped = jnp.clip(y_pred, self.min_val, self.max_val)
+        
+        if len(y_true.shape) == 1:
+            calculated_confidences = y_pred_clipped[range(y_true.shape[0]), y_true]
+        else:
+            calculated_confidences = jnp.sum(y_true * y_pred_clipped, axis=1)
+        
+        negative_log_likelihoods = -jnp.log(calculated_confidences)
+        return negative_log_likelihoods
---
+++ loss/loss.py
@@ -0,0 +1,15 @@
+import jax.numpy as jnp
+
+from abc import ABC, abstractmethod
+from jax import Array
+from jax.typing import ArrayLike
+
+class Loss(ABC):
+    def __call__(self, y_pred: Array, y_true: Array) -> ArrayLike:
+        calculated_loss:ArrayLike = self.calculate_loss(y_pred, y_true)
+        loss = jnp.mean(calculated_loss)
+        return loss
+    
+    @abstractmethod
+    def calculate_loss(self, y_pred: Array, y_true: Array) -> Array:
+        raise NotImplementedError("Subclasses must implement calculate_loss method.")
