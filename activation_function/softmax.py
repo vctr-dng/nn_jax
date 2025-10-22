softmax fix for batch

activation_function/softmax.py
@@ -6,5 +6,5 @@ from activation_function import ActivationFunction
 
 class Softmax(ActivationFunction):
     def forward(self, inputs: ArrayLike) -> ArrayLike:
-        exps = jnp.exp(inputs - jnp.max(inputs))
+        exps = jnp.exp(inputs - jnp.max(inputs, axis=1, keepdims=True))
         return exps / jnp.sum(exps, axis=1, keepdims=True)
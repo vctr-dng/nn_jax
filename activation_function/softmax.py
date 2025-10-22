470392ed8bdeda4198ca0883ca01927e9e087fd8
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2025-10-21 23:21:54 -0400

    softmax fix for batch

--- activation_function/softmax.py
+++ activation_function/softmax.py
@@ -6,5 +6,5 @@ from activation_function import ActivationFunction
 
 class Softmax(ActivationFunction):
     def forward(self, inputs: ArrayLike) -> ArrayLike:
-        exps = jnp.exp(inputs - jnp.max(inputs))
+        exps = jnp.exp(inputs - jnp.max(inputs, axis=1, keepdims=True))
         return exps / jnp.sum(exps, axis=1, keepdims=True)
