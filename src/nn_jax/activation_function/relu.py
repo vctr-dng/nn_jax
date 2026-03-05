aca5db5b7fb57d785cdb382c80799463e0fbf719
vdang <68-vdang@users.noreply.gitlab.formule>
2026-03-05 01:40:42 -0500

    fix: follow jax type hinting best practices

--- src/nn_jax/activation_function/relu.py
+++ src/nn_jax/activation_function/relu.py
@@ -1,9 +1,10 @@
 import jax.numpy as jnp
+from jax import Array
 from jax.typing import ArrayLike
 
 from .activation_function import ActivationFunction
 
 
 class ReLU(ActivationFunction):
-    def forward(self, inputs: ArrayLike) -> ArrayLike:
+    def forward(self, inputs: ArrayLike) -> Array:
         return jnp.maximum(0, inputs)
