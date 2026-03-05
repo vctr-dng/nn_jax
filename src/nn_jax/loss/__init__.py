563524fcc83c36d7f873ee99bbaf7f4079107d81
vdang <68-vdang@users.noreply.gitlab.formule>
2026-03-05 01:39:45 -0500

    chore: class reference instead of str

--- src/nn_jax/loss/__init__.py
+++ src/nn_jax/loss/__init__.py
@@ -1,4 +1,4 @@
 from nn_jax.loss.categorial_cross_entropy import CategorialCrossEntropy
 from nn_jax.loss.loss import Loss
 
-__all__ = ["CategorialCrossEntropy", "Loss"]
+__all__ = [Loss, CategorialCrossEntropy]
