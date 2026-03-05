7bd558ad945c456e4cfb6f0014c79ae0698b5056
vdang <68-vdang@users.noreply.gitlab.formule>
2026-03-05 01:32:52 -0500

    fix: remove vmap in dense forward method for simple Ax+b, follow jax type hinting best practices

--- src/nn_jax/layer/dense.py
+++ src/nn_jax/layer/dense.py
@@ -1,9 +1,8 @@
-from functools import partial
-from typing import Callable
-
 import jax.numpy as jnp
 import jax.random as random
-from jax import Array, vmap
+
+from jax import Array, tree_util
+from jax.typing import ArrayLike
 
 from .layer import Layer
 
@@ -19,22 +18,39 @@ class Dense(Layer):
         self.weights: Array = 0.1 * random.normal(
             key, shape=(self.output_size, self.input_size), dtype=self.weight_type
         )
-        self.biases: Array = jnp.zeros(self.output_size, dtype=self.weight_type)
-        self.output: Array = self.forward(jnp.zeros((1, self.input_size)))
+        self.biases: ArrayLike = jnp.zeros(self.output_size, dtype=self.weight_type)
+        self.output: Array = self.forward(jnp.zeros(self.input_size))
 
-    def forward(self, inputs: Array) -> Array:
-        partial_forward: Callable[[Array], Array] = partial(
-            single_forward, self.weights, self.biases
-        )
-
-        # vmap transforms partial_forward to work on batches
-        # in_axes=0: iterate over the first axis (batch dimension) of inputs
-        # out_axes=0: stack results along the first axis (batch dimension)
-        vectorized_forward = vmap(partial_forward, in_axes=0, out_axes=0)
-
-        self.output = vectorized_forward(inputs)
+    def forward(self, input: ArrayLike) -> Array:
+        self.output = self.weights @ input + self.biases
         return self.output
 
+    def _tree_flatten(self):
+        dynamic_values = (
+            self.weights,
+            self.biases,
+            self.output,
+        )
+        static_values = {
+            "input_size": self.input_size,
+            "output_size": self.output_size,
+        }
+        return (dynamic_values, static_values)
 
-def single_forward(weights: Array, biases: Array, x: Array) -> Array:
-    return weights @ x + biases
+    @classmethod
+    def _tree_unflatten(cls, static_values, dynamic_values):
+        dense = cls(
+            static_values["input_size"],
+            static_values["output_size"],
+        )
+        dense.weights = dynamic_values[0]
+        dense.biases = dynamic_values[1]
+        dense.output = dynamic_values[2]
+        return dense
+
+
+tree_util.register_pytree_node(
+    Dense,
+    Dense._tree_flatten,
+    Dense._tree_unflatten,
+)
