e07e376f1ea6f72c3d74cfb2c312ddb98f5f9871
vdang <68-vdang@users.noreply.gitlab.formule>
2026-03-05 01:34:10 -0500

    feature: pytree for every activation function

--- src/nn_jax/activation_function/__init__.py
+++ src/nn_jax/activation_function/__init__.py
@@ -1,5 +1,14 @@
+from jax import tree_util
+
 from nn_jax.activation_function.activation_function import ActivationFunction
 from nn_jax.activation_function.relu import ReLU
 from nn_jax.activation_function.softmax import Softmax
 
-__all__ = ["ActivationFunction", "ReLU", "Softmax"]
+__all__ = [ActivationFunction, ReLU, Softmax]
+
+for activation_function in __all__:
+    tree_util.register_pytree_node(
+        activation_function,
+        activation_function._tree_flatten,
+        activation_function._tree_unflatten,
+    )
