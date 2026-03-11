e9fd1afa73e6bcc59b2933824afa47912d61e0a5
Victor <94930828+vctr-dng@users.noreply.github.com>
2026-03-11 00:17:12 -0400

    Fix categorical typo (#8)
    * chore: update pre-commit hooks
    
    * fix: typo

--- src/nn_jax/loss/__init__.py
+++ src/nn_jax/loss/__init__.py
@@ -1,4 +1,4 @@
-from nn_jax.loss.categorial_cross_entropy import CategorialCrossEntropy
+from nn_jax.loss.categorical_cross_entropy import CategoricalCrossEntropy
 from nn_jax.loss.loss import Loss
 
-__all__ = [Loss, CategorialCrossEntropy]
+__all__ = [Loss, CategoricalCrossEntropy]
