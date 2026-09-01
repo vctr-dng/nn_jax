ee6ff9ad69800a39d58f65729f9cb2846cb9d977
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2026-09-01 01:10:39 -0400

    feat: add global average pooling in pooling importables

--- src/nn_jax/pooling/__init__.py
+++ src/nn_jax/pooling/__init__.py
@@ -1,5 +1,6 @@
 from .average_pool2d import AvgPool2D
+from .global_average_pooling2d import GlobalAvgPool2D
 from .max_pool2d import MaxPool2D
 from .pooling import Pooling
 
-__all__ = ["AvgPool2D", "MaxPool2D", "Pooling"]
+__all__ = ["AvgPool2D", "GlobalAvgPool2D", "MaxPool2D", "Pooling"]
